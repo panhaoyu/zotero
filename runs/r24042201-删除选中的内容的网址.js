01181f98e50a7cd5fb10b8505663f9fa044db0ca
panhaoyu <panhaoyu.china@outlook.com>
2024-04-22 15:50:48 +0800

    add: r24042201-删除选中的内容的网址.js

--- runs/r24042201-删除选中的内容的网址.js
+++ runs/r24042201-删除选中的内容的网址.js
@@ -1,26 +1,32 @@
-// 这段代码需要在Zotero的环境中运行
-async function removeURLsFromSelectedItems() {
-    // 获取当前选中的所有文献
-    let items = Zotero.getActiveZoteroPane().getSelectedItems();
+async function script(item) {
+    if (!item) {
+        return "No item"
+    }
 
-    // 遍历每个选中的文献
-    for (let item of items) {
-        // 确保当前对象是一个文献条目
-        if (item.isRegularItem() && !item.isAttachment() && !item.isNote()) {
-            // 访问文献的网址字段
-            let urlField = item.getField('url');
+// 确保当前对象是一个文献条目
+    if (!item.isRegularItem()) {
+        return
+    }
+    if (item.isAttachment()) {
+        return
+    }
+    if (item.isNote) {
+        return
+    }
 
-            // 检查是否存在网址
-            if (urlField) {
-                // 删除网址
-                item.setField('url', '');
+    let urlField = item.getField('url');
 
-                // 保存更改
-                await item.saveTx();
-            }
-        }
+
+    const prefix = 'https://webofscience.clarivate.cn/wos/alldb/summary/'
+
+// 检查是否存在网址
+    if (urlField.startsWith(prefix) {
+        // 删除网址
+        item.setField('url', '');
+
+        // 保存更改
+        await item.saveTx();
     }
 }
 
-// 调用函数
-removeURLsFromSelectedItems();
+await script(item)
