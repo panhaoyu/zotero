bc4ed6a002b7e67d1477a40c418f0a20280c17f2
panhaoyu <panhaoyu.china@outlook.com>
2024-04-27 19:05:39 +0800

    add: r24042701-识别NoteExpress中导入的内容.js

--- runs/r24042701-识别NoteExpress中导入的内容.js
+++ runs/r24042701-识别NoteExpress中导入的内容.js
@@ -1,20 +1,12 @@
-// 对以下内容进行持续的修改。
-// 不要使用Zotero.debug，而是要使用console.log
-if (ZoteroPane.getSelectedItems().length) {
-    const item = ZoteroPane.getSelectedItems()[0];
+/*
+你将写一段可以在Zotero本地客户端里面的Run Javascript功能里面直接可以运行的代码。
+对以下代码进行修改，根据todo里面的内容进行修改。
+遵循以下要求：
+- 不要使用Zotero.debug而是要使用console.log进行日志的输出。
+ */
+for (const item of ZoteroPane.getSelectedItems()) {
     const notes = item.getNotes().map(i => Zotero.Items.get(i).getNote())
     console.log(notes)
-
-    // 获取所有子项，这些子项是笔记
-    // Zotero.Items.getAsync(item.id).then(item => item.getNotes()).then(notesIds => {
-    //     return Zotero.Items.getAsync(notesIds);
-    // }).then(notesItems => {
-    //     notesItems.forEach(note => {
-    //         // 输出笔记内容
-    //         console.log("笔记内容: " + note.getNote());
-    //     });
-    // });
-} else {
-    ZoteroPane.displayErrorMessage("没有选中的条目");
+    // todo 每一个笔记的格式都如下所示，对其进行调整，转换为纯文本，删除格式标记。
+    // <p>NoteExpress Custom Data<br/>Tags: 热; EGS; 结构面; 目录-粗糙度<br/>Rating: 3<br/>NoteExpress ID: 1417</p>
 }
-
