ae387fd04d9656cd6c9d51c5031ea51eb1521b71
panhaoyu <panhaoyu.china@outlook.com>
2024-04-27 21:08:01 +0800

    add: r24042701-识别NoteExpress中导入的内容.js

--- runs/r24042701-识别NoteExpress中导入的内容.js
+++ runs/r24042701-识别NoteExpress中导入的内容.js
@@ -7,6 +7,37 @@
 - 避免使用正则表达式
 - 只需要给我发生变动的代码。对于不相关的代码不必重复。
  */
+
+/*
+示例的RIS文件的题录格式如下，可以对NoteExpress的模板进行编辑，主要是添加一个新的N1字段，也就是下面的示例里面的最后一个字段。
+这样就可以通过读取该字段中的标题实现相应的文件匹配。
+需要先将文件借助NoteExpress重命名为相应的格式，即文献编号 序号格式。
+从而可以实现文件、标签、评级的导入。
+
+TY  - JOUR
+AU  - Author A
+AU  - Author B
+AU  - Author C
+PY  - 2018
+TI  - This is the title
+SP  - 2219-2231
+JF  - The journal name
+VL  - 37
+IS  - 10
+SN  - 1000-6915
+M3  - 10.0000/a.b.c.e.f.g
+N2  - This is the abstract.
+AD  - xxx; xxx
+UR  - https://link.to/paper
+DO  - 10.0000/a.b.c.e.f.g
+N1  - NoteExpress Custom Data
+Tags: 渗流剪切; 目录-渗流剪切
+Rating: 0
+NoteExpress ID: 169
+ER  -
+
+
+ */
 (async () => {
     const baseDir = 'F:\\OneDrive\\1002-data\\24041201-note-express\\rock.Attachments\\'
     const fileStats = {};
