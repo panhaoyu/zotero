d236f9eeced9442a7f8a3ee77cd5ebd89adef50d
panhaoyu <panhaoyu.china@outlook.com>
2024-04-27 20:07:41 +0800

    add: r24042701-识别NoteExpress中导入的内容.js

--- runs/r24042701-识别NoteExpress中导入的内容.js
+++ runs/r24042701-识别NoteExpress中导入的内容.js
@@ -5,8 +5,21 @@
 - 不要使用Zotero.debug而是要使用console.log进行日志的输出。
 - 不必向我解释编码的过程，不要先为我解释要做什么事情，准备做什么事情，而是直接给我代码。
 - 避免使用正则表达式
+- 只需要给我发生变动的代码。对于不相关的代码不必重复。
  */
 (async () => {
+    const baseDir = 'F:\\OneDrive\\1002-data\\24041201-note-express\\rock.Attachments\\'
+    const files = []
+    const fileStats = {};
+    await Zotero.File.iterateDirectory(baseDir, async i => {
+        files.push(i)
+        const fileNameParts = i.name.split(' '); // Assumes file names are separated by spaces and the first part is the numeric key
+        const key = fileNameParts[0]; // The first part of the filename, expected to be numeric
+        if (!fileStats[key]) {
+            fileStats[key] = [];
+        }
+        fileStats[key].push(i);
+    })
     for (const item of ZoteroPane.getSelectedItems()) {
         const notes = item.getNotes().map(i => {
             const noteItem = Zotero.Items.get(i)
@@ -49,5 +62,12 @@
         await Zotero.DB.executeTransaction(async () => {
             await item.save();
         });
+
+        try {
+            const v = await Zotero.File.iterateDirectory(baseDir, true)
+            console.log(v)
+        } catch (error) {
+            console.log('Error accessing directory:', error);
+        }
     }
 })().then()
